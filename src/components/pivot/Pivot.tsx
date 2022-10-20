9967f384ff34c4d10dae652999c27db66a20835d
James Zhang <liulfbaby@gmail.com>
2022-10-20 12:48:17 +0800

    fix: add nearest syncfusion component.

--- src/components/pivot/Pivot.tsx
+++ src/components/pivot/Pivot.tsx
@@ -1,16 +1,30 @@
-import { PivotViewComponent } from '@syncfusion/ej2-react-pivotview';
-import * as localData from './localdata.json'
+import { IDataOptions, PivotViewComponent } from '@syncfusion/ej2-react-pivotview';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
 export function Pivot() {
+  const remoteData = new DataManager({
+    url: 'https://bi.syncfusion.com/northwindservice/api/orders',
+    adaptor: new WebApiAdaptor,
+    crossDomain: true
+  });
+
+  const dataSourceSettings: IDataOptions = {
+    dataSource: remoteData,
+    type: 'JSON',
+    expandAll: true,
+    filters: [],
+    columns: [{ name: 'ProductName', caption: 'Product Name' }],
+    rows: [{ name: 'ShipCountry', caption: 'Ship Country' }, { name: 'ShipCity', caption: 'Ship City' }],
+    formatSettings: [{ name: 'UnitPrice', format: 'C0' }],
+    values: [{ name: 'Quantity' }, { name: 'UnitPrice', caption: 'Unit Price' }],
+  };
+
   return (
-    <div className={"app"}>
+    <div className={'app'}>
       <PivotViewComponent
-        dataSourceSettings={{
-          dataSource: localData.data,
-          values: [{ name: 'Sold' }, { name: 'Amount' }],
-          rows: [{ name: 'Country' }, { name: 'Products' }],
-          columns: [{ name: 'Year' }, { name: 'Quarter' }]
-        }}
+        id={'PivotView'}
+        width={'100%'}
+        dataSourceSettings={dataSourceSettings}
       >
       </PivotViewComponent>
     </div>
