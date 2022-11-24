2ea6cb69d6565439f0f5308b67b3c01f2aac50af
James Zhang <liulfbaby@gmail.com>
2022-11-24 20:08:52 +0800

    feat: create a createStore function

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
+export const createStore = () => configureStore({
   reducer: {
     counter: counterReducer,
   },
 });
+
+export const store = createStore();
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
--- src/components/Counter.spec.tsx
+++ src/components/Counter.spec.tsx
@@ -1,10 +1,10 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Counter from './Counter';
 import { Provider } from 'react-redux';
-import { store } from '../app/store';
+import { createStore } from '../app/store';
 
 function renderWithStore() {
-  const Wrapper = () => <Provider store={store}>
+  const Wrapper = () => <Provider store={createStore()}>
     <Counter />
   </Provider>;
   render(<Wrapper />);
@@ -20,6 +20,6 @@ describe('Counter', function() {
   it('should decrement number after clicking button', function() {
     renderWithStore();
     fireEvent.click(screen.getByRole('button', { name: 'Decrement value' }));
-    expect(screen.getByTitle('counter')).toHaveTextContent('0');
+    expect(screen.getByTitle('counter')).toHaveTextContent('-1');
   });
 });
