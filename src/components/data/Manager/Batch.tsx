ec44f43b5c40b1f3eee437a1f7acee8b710037bf
James Zhang <liulfbaby@gmail.com>
2022-11-27 19:43:46 +0800

    feat: DataManager实现batch已经完整工作

--- src/components/data/Manager/Batch.tsx
+++ src/components/data/Manager/Batch.tsx
@@ -1,5 +1,5 @@
 import { DataManager, Query, ReturnOption } from '@syncfusion/ej2-data';
-import { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { data } from '../../grid/DataSource';
 
 function Row(props: IOrder) {
@@ -16,7 +16,8 @@ export const Batch = () => {
   let dm: DataManager;
   let style: { [x: string]: string; } = { class: 'e-form' };
   const [state, setState] = useState<{ items: IOrder[] } | undefined>(undefined);
-  dm = new DataManager(data.slice(0, 5));
+  const [dataSource] = useState(data.slice(0, 5));
+  dm = new DataManager(dataSource);
   const [action, setAction] = useState('Insert');
   const [changes, setChanges] = useState<ChangedRecordsProps>(
     {
@@ -25,6 +26,9 @@ export const Batch = () => {
       deletedRecords: [],
     },
   );
+  const [orderID, setOrderID] = useState('');
+  const [customerID, setCustomerID] = useState('');
+  const [employeeID, setEmployeeID] = useState('');
 
   function createRow(order: IOrder[]) {
     return order.map((row) => <Row key={row.OrderID} {...row} />);
@@ -36,56 +40,53 @@ export const Batch = () => {
     });
   }, []);
 
-  const wrappedAction = useCallback(async () => {
-      const orderId = document.getElementById('OrderID') as HTMLInputElement;
-      const cusId = document.getElementById('CustomerID') as HTMLInputElement;
-      const empId = document.getElementById('EmployeeID') as HTMLInputElement;
-      const rowData: IOrder = {
-        CustomerID: cusId.value,
-        EmployeeID: Number(empId.value),
-        OrderID: Number(orderId.value),
-      };
-      console.log('rowData', rowData);
-      if (!rowData.OrderID) return;
-      orderId.value = cusId.value = empId.value = '';
-      if (action === 'Remove') {
-        await setChanges(prevState => {
-          console.log('In Remove prevState', prevState);
-          const { deletedRecords } = prevState;
-          deletedRecords.push(rowData);
-          return Object.assign({}, prevState, {
-            deletedRecords: deletedRecords,
-          });
+  const wrappedAction = () => {
+    const rowData: IOrder = {
+      CustomerID: customerID,
+      EmployeeID: Number(employeeID),
+      OrderID: Number(orderID),
+    };
+    console.log('rowData', rowData);
+    setCustomerID('');
+    setOrderID('');
+    setEmployeeID('');
+    if (action === 'Remove') {
+      setChanges(prevState => {
+        console.log('In Remove prevState', prevState);
+        const { deletedRecords } = prevState;
+        deletedRecords.push(rowData);
+        return Object.assign({}, prevState, {
+          deletedRecords: deletedRecords,
         });
-        console.log('In Remove', changes);
-      } else if (action === 'Update') {
-        console.log('In Update before', changes);
-        await setChanges(prevState => {
-          const { changedRecords } = prevState;
-          changedRecords.push(rowData);
-          return Object.assign({}, prevState, {
-            changedRecords: changedRecords,
-          });
+      });
+      console.log('In Remove', changes);
+    } else if (action === 'Update') {
+      console.log('In Update before', changes);
+      setChanges(prevState => {
+        const { changedRecords } = prevState;
+        changedRecords.push(rowData);
+        return Object.assign({}, prevState, {
+          changedRecords: changedRecords,
         });
-        console.log('In Update', changes);
-      } else {
-        const { addedRecords } = changes;
-        addedRecords.push(rowData);
-        await setChanges(prevState => {
-          return Object.assign({}, prevState, { addedRecords: addedRecords });
-        });
-        console.log('In Add', changes);
-      }
+      });
+      console.log('In Update', changes);
+    } else {
+      const { addedRecords } = changes;
+      addedRecords.push(rowData);
+      setChanges(prevState => {
+        return Object.assign({}, prevState, { addedRecords: addedRecords });
+      });
+      console.log('In Add', changes);
     }
-    , [action]);
+  };
 
   function onSelectAction(e: ChangeEvent<HTMLSelectElement>) {
     setAction(e.target.value);
   }
 
-  async function onSaveChanges() {
-    dm.saveChanges(changes);
-    await setChanges(prevState => {
+  function onSaveChanges() {
+    dm.saveChanges(changes, 'OrderID');
+    setChanges(prevState => {
       const newChanges = {
         addedRecords: [],
         deletedRecords: [],
@@ -95,6 +96,7 @@ export const Batch = () => {
     });
     dm.executeQuery(new Query()).then((e: ReturnOption) => {
       console.log('In ExecuteQuery', changes);
+      console.log('eeeee', e);
       setState({
         items: (e.result as IOrder[]),
       });
@@ -109,9 +111,12 @@ export const Batch = () => {
         <option value='Update'>Update</option>
         <option value='Remove'>Remove</option>
       </select>
-      <input type='number' id='OrderID' placeholder='Order ID' />
-      <input type='text' id='CustomerID' placeholder='Customer ID' />
-      <input type='number' id='EmployeeID' placeholder='Employee ID' />
+      <input type='number' value={orderID} onChange={(e) => setOrderID(e.target.value)} id='OrderID'
+             placeholder='Order ID' />
+      <input type='text' value={customerID} onChange={(e) => setCustomerID(e.target.value)} id='CustomerID'
+             placeholder='Customer ID' />
+      <input type='number' value={employeeID} onChange={(e) => setEmployeeID(e.target.value)} id='EmployeeID'
+             placeholder='Employee ID' />
       <input className='px-2 py-1 rounded' type='button' value='Action' id='manipulate' onClick={wrappedAction} />
       <button className='px-2 py-1 rounded ml-3' onClick={onSaveChanges}>Save Changes</button>
     </div>
