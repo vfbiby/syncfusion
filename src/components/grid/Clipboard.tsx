62bb5d35d3cc401619a8e60aaf69327d936a35a8
James Zhang <liulfbaby@gmail.com>
2022-11-28 11:25:09 +0800

    feat: add clipboard add contextMenu

--- src/components/grid/Clipboard.tsx
+++ src/components/grid/Clipboard.tsx
@@ -1,6 +1,12 @@
-import { ColumnDirective, ColumnsDirective, GridComponent } from '@syncfusion/ej2-react-grids';
+import { ColumnDirective, ColumnsDirective, GridComponent, Inject } from '@syncfusion/ej2-react-grids';
 import { data } from '../../datasource';
 import { useRef } from 'react';
+import { ContextMenu, ContextMenuItem } from '@syncfusion/ej2-grids';
+
+export const contextMenuItems: ContextMenuItem[] = ['AutoFit', 'AutoFitAll',
+  'SortAscending', 'SortDescending', 'Copy', 'Edit', 'Delete', 'Save',
+  'Cancel', 'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
+  'LastPage', 'NextPage'];
 
 export const Clipboard = () => {
   const gridRef = useRef<GridComponent | null>(null);
@@ -14,15 +20,23 @@ export const Clipboard = () => {
   };
 
   return <div>
-    <button className="px-2 py-1 rounded m-2" onClick={copy}>Copy</button>
-    <button className="px-2 py-1 rounded m-2" onClick={copyHeader}>CopyHeader</button>
-    <GridComponent ref={gridRef} dataSource={data} selectionSettings={{ type: 'Multiple' }} height={272}>
+    <button className='px-2 py-1 rounded m-2' onClick={copy}>Copy</button>
+    <button className='px-2 py-1 rounded m-2' onClick={copyHeader}>CopyHeader</button>
+    <GridComponent
+      ref={gridRef}
+      showColumnMenu
+      contextMenuItems={contextMenuItems}
+      dataSource={data}
+      selectionSettings={{ type: 'Multiple' }}
+      height={272}
+    >
       <ColumnsDirective>
         <ColumnDirective field='OrderID' headerText='Order ID' width='120' textAlign='Right' />
         <ColumnDirective field='CustomerID' headerText='Customer ID' width='150' />
         <ColumnDirective field='ShipCity' headerText='Ship City' width='150' />
         <ColumnDirective field='ShipName' headerText='Ship Name' width='150' />
       </ColumnsDirective>
+      <Inject services={[ContextMenu]} />
     </GridComponent>
   </div>;
 };
