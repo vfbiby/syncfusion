472f688c3c344f2e32a9754fe3b2c9dc49d7001f
James Zhang <liulfbaby@gmail.com>
2022-11-28 11:37:27 +0800

    feat: add batch cell editing.

--- src/components/grid/Clipboard.tsx
+++ src/components/grid/Clipboard.tsx
@@ -1,7 +1,13 @@
-import { ColumnDirective, ColumnsDirective, GridComponent, Inject } from '@syncfusion/ej2-react-grids';
+import { ColumnDirective, ColumnsDirective, GridComponent, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
 import { data } from '../../datasource';
-import { useRef } from 'react';
-import { ContextMenu, ContextMenuItem } from '@syncfusion/ej2-grids';
+import { useRef, useState } from 'react';
+import {
+  ContextMenu,
+  ContextMenuItem, Edit,
+  EditSettingsModel,
+  SelectionSettingsModel,
+  ToolbarItems,
+} from '@syncfusion/ej2-grids';
 
 export const contextMenuItems: ContextMenuItem[] = ['AutoFit', 'AutoFitAll',
   'SortAscending', 'SortDescending', 'Copy', 'Edit', 'Delete', 'Save',
@@ -10,6 +16,9 @@ export const contextMenuItems: ContextMenuItem[] = ['AutoFit', 'AutoFitAll',
 
 export const Clipboard = () => {
   const gridRef = useRef<GridComponent | null>(null);
+  const toolbarOptions: ToolbarItems[] = ['Add', 'Update', 'Cancel'];
+  const editOptions: EditSettingsModel = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Batch' };
+  const selectionOptions: SelectionSettingsModel = { cellSelectionMode: 'Box', type: 'Multiple', mode: 'Cell' };
 
   const copy = () => {
     gridRef.current?.copy();
@@ -25,18 +34,21 @@ export const Clipboard = () => {
     <GridComponent
       ref={gridRef}
       showColumnMenu
+      editSettings={editOptions}
+      toolbar={toolbarOptions}
       contextMenuItems={contextMenuItems}
       dataSource={data}
-      selectionSettings={{ type: 'Multiple' }}
+      selectionSettings={selectionOptions}
+      enableAutoFill
       height={272}
     >
       <ColumnsDirective>
-        <ColumnDirective field='OrderID' headerText='Order ID' width='120' textAlign='Right' />
+        <ColumnDirective isPrimaryKey visible={false} field='OrderID' headerText='Order ID' width='120' textAlign='Right' />
         <ColumnDirective field='CustomerID' headerText='Customer ID' width='150' />
         <ColumnDirective field='ShipCity' headerText='Ship City' width='150' />
         <ColumnDirective field='ShipName' headerText='Ship Name' width='150' />
       </ColumnsDirective>
-      <Inject services={[ContextMenu]} />
+      <Inject services={[ContextMenu, Edit, Toolbar]} />
     </GridComponent>
   </div>;
 };
