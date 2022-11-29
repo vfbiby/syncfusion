7adc482c6290a687cfd28288890ba7512480f2eb
James Zhang <liulfbaby@gmail.com>
2022-11-29 20:58:58 +0800

    feat: Add native clipboard code.

--- src/components/data/CopyToClipboard.tsx
+++ src/components/data/CopyToClipboard.tsx
@@ -2,15 +2,26 @@ import { useCopyToClipboard } from 'usehooks-ts';
 
 export function CopyToClipboard() {
   const [value, copy] = useCopyToClipboard();
+
+  const copyFromClipboard = () => {
+    navigator.clipboard.readText().then((context) => {
+      console.log(context);
+    });
+  };
+
   return (
     <>
       <h1>Click to copy:</h1>
-      <div className="flex justify-center gap-3">
+      <div className='flex justify-center gap-3'>
         <button className='px-2 py-1 rounded' onClick={() => copy('A')}>A</button>
         <button className='px-2 py-1 rounded' onClick={() => copy('B')}>B</button>
         <button className='px-2 py-1 rounded' onClick={() => copy('C')}>C</button>
       </div>
       <p>Copied value: {value ?? 'Nothing is copied yet!'}</p>
+      <hr />
+      <div>
+        <button onClick={copyFromClipboard} className='p-2 rounded m-2'>Native Copy</button>
+      </div>
     </>
   );
 }
