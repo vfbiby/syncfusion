d5e96935d3234d7237c3d231866412454d5b614d
James Zhang <liulfbaby@gmail.com>
2022-12-02 20:55:38 +0800

    feat: 添加contextMenu

--- src/components/data/Manager/ManagerLocal.tsx
+++ src/components/data/Manager/ManagerLocal.tsx
@@ -1,16 +1,24 @@
-import { DataManager, JsonAdaptor, Query } from '@syncfusion/ej2-data';
+import { DataManager, JsonAdaptor } from '@syncfusion/ej2-data';
 import { data } from '../../../datasource';
 import {
   ColumnDirective,
-  ColumnsDirective, Edit, EditSettingsModel,
-  GridComponent,
+  ColumnMenu,
+  ColumnsDirective, ContextMenu, ContextMenuItem, ContextMenuItemModel,
+  Edit,
+  EditSettingsModel,
+  Filter,
+  FilterSettingsModel,
+  GridComponent, Group, GroupSettingsModel,
   Inject,
   Page,
   Sort,
-  SortSettingsModel, Toolbar, ToolbarItems,
+  SortSettingsModel,
+  Toolbar,
+  ToolbarItems,
 } from '@syncfusion/ej2-react-grids';
 import { Order } from '../../../app/Type';
 import { useRef } from 'react';
+import { MenuEventArgs } from '@syncfusion/ej2-navigations';
 
 const dm = new DataManager({ json: data, adaptor: new JsonAdaptor });
 const sortSettings: SortSettingsModel = {
@@ -18,6 +26,14 @@ const sortSettings: SortSettingsModel = {
 };
 const editOptions: EditSettingsModel = { allowEditing: true, allowDeleting: true, allowAdding: true, mode: 'Batch' };
 const toolbarOptions: ToolbarItems[] = ['Add', 'Update', 'Delete', 'Edit'];
+const filterSettings: FilterSettingsModel = { type: 'CheckBox' };
+const groupOptions: GroupSettingsModel = { showGroupedColumn: true, showDropArea: false };
+const contextMenuItems: (ContextMenuItem | ContextMenuItemModel)[] = ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending', 'Copy', 'Edit', 'Delete', 'Save',
+  'Cancel', 'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage', 'LastPage', 'NextPage', {
+    text: 'Copy with headers',
+    id: 'copyWithHeader',
+    target: '.e-content',
+  }];
 
 export const ManagerLocal = () => {
   const gridRef = useRef<GridComponent | null>(null);
@@ -50,6 +66,12 @@ export const ManagerLocal = () => {
     return <div><input className='px-2 py-1 rounded' defaultValue={rows.ShipCity} /></div>;
   };
 
+  const contextMenuClick = (args: MenuEventArgs) => {
+    if (gridRef.current && args.item.id === 'copyWithHeader') {
+      gridRef.current.copy(true);
+    }
+  };
+
   return <div>
     <div>
       <button onClick={add} className='p-2 m-2 rounded'>DM Add</button>
@@ -59,15 +81,19 @@ export const ManagerLocal = () => {
       <button onClick={setCell} className='p-2 m-2 rounded'>Set Cell</button>
       <button onClick={setRow} className='p-2 m-2 rounded'>Set Row</button>
     </div>
-    <GridComponent ref={gridRef} allowSorting allowPaging toolbar={toolbarOptions} editSettings={editOptions}
-                   sortSettings={sortSettings} dataSource={dm}>
+    <GridComponent
+      allowSorting allowPaging allowFiltering showColumnMenu allowGrouping
+      // @ts-ignore
+      filterSettings={filterSettings} ref={gridRef} groupSettings={groupOptions} contextMenuItems={contextMenuItems}
+      toolbar={toolbarOptions} editSettings={editOptions} sortSettings={sortSettings} dataSource={dm}>
       <ColumnsDirective>
-        <ColumnDirective field='OrderID' isPrimaryKey={true} />
-        <ColumnDirective field='CustomerID' />
+        <ColumnDirective field='OrderID' allowEditing={false} isPrimaryKey={true} />
+        <ColumnDirective field='CustomerID' allowFiltering={false} />
+        <ColumnDirective field='EmployeeID' editType='numericedit' />
         <ColumnDirective field='ShipRegion' />
         <ColumnDirective headerText='City' template={getCustomTemplate} />
       </ColumnsDirective>
-      <Inject services={[Sort, Toolbar, Edit, Page]} />
+      <Inject services={[Sort, Toolbar, ColumnMenu, ContextMenu, Group, Filter, Edit, Page]} />
     </GridComponent>
   </div>;
 };
